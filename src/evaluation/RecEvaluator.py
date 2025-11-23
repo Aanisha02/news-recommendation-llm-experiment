e8f3e8a50683b21831413916467340b306bb3dc9
Aanisha Newaz <90078094+Aanisha02@users.noreply.github.com>
2025-11-23 15:10:13 -0500

    Update RecEvaluator.py

--- src/evaluation/RecEvaluator.py
+++ src/evaluation/RecEvaluator.py
@@ -34,6 +34,11 @@ class RecEvaluator:
         # ref(MIND Official): https://github.com/msnews/MIND/blob/master/evaluate.py#L7-L12
         # ref(sklearn): https://github.com/scikit-learn/scikit-learn/blob/7f9bad99d6e0a3e8ddf92a7e5561245224dab102/sklearn/metrics/_ranking.py#L1444-L1458
 
+        if len(y_true) == 0:
+            return 0.0
+
+        K = min(K, len(y_true))   # <-- ADD THIS LINE
+
         discounts = np.log2(np.arange(len(y_true)) + 2)[:K]
 
         y_score_rank = np.argsort(y_score)[::-1]
@@ -45,10 +50,13 @@ class RecEvaluator:
     @classmethod
     def ndcg_score(cls, y_true: np.ndarray, y_score: np.ndarray, K: int = 5) -> float:
         best = cls.dcg_score(y_true, y_true, K)
+        if best == 0:
+            return 0.0
         actual = cls.dcg_score(y_true, y_score, K)
         return actual / best
 
     @classmethod
+    
     def mrr_score(cls, y_true: np.ndarray, y_score: np.ndarray) -> float:
         y_score_rank = np.argsort(y_score)[::-1]
         y_true_sorted_by_y_score = np.take(y_true, y_score_rank)
